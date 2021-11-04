323732b5ec557c6d6a2f28a9cbc94481897c597c
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-11-04 11:21:26 -0500

    added basic logic for starting and ending the quiz, timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,48 @@ var choicesElement = document.getElementById("choices");
 var submitElement = document.getElementById("submit");
 var nameElement = document.getElementById("enter-name");
 var feedbackElement = document.getElementById("feedback");
+
+const startQuiz = () => {
+    // add attributes to hide start screen
+    var startScreenElement = document.getElementById("start-screen");
+    startScreenElement.setAttribute("class", "hide");
+
+    // remove attribute to show questions 
+    questionsElement.removeAttribute("class");
+
+    // begin timer for 10 seconds
+    timerID = setInterval(clockTick, 1000); 
+
+    // show starting time
+    timerElement.textContent = time;
+
+    getQuestion();
+}
+
+const endQuiz = () => {
+    // end timer 
+    clearInterval(timerID);
+
+    // remove attribute to show end screen 
+    var endScreenElement = document.getElementById("end-screen");
+    endScreenElement.removeAttribute("class");
+
+    // remove attribute to show final score 
+    var finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    // add attributes to hide questions section 
+    questionsElement.setAttribute("class", "hide");
+}
+
+const clockTick = () => {
+    // update time 
+    time--;
+    timerElement.textContent = time;
+
+    // end quiz if user runs out of time 
+    if (time <= 0) {
+        endQuiz();
+    }
+}
+
